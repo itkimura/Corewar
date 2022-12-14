296ce321dd7c771ecdef7b6346f3de083aa4ba84
Clement Cariou <wdclem@hotmail.fr>
2022-12-14 14:30:10 +0200

    atoi base add

---
+++ ft_atoi_base.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccariou <ccariou@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 14:28:28 by ccariou           #+#    #+#             */
+/*   Updated: 2022/12/14 14:29:52 by ccariou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+static int	ft_iswhitespace(char const c)
+{
+	if (c == ' ' || c == '\n' || c == '\t' || c == '\v'
+		|| c == '\r' || c == '\f')
+		return (1);
+	return (0);
+}
+
+static int	convert(int c, int base)
+{
+	char	*str;
+	char	*str2;
+	int		i;
+
+	str = "0123456789abcdef";
+	str2 = "0123456789ABCDEF";
+	i = 0;
+	while (i < base)
+	{
+		if (c == str[i] || c == str2[i])
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	ft_atoi_base(const char *str, int base)
+{
+	int	nb;
+	int	isneg;
+	int	i;
+
+	i = 0;
+	isneg = 0;
+	nb = 0;
+	while (ft_iswhitespace(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			isneg = 1;
+		i++;
+	}
+	while (base(str[i], base) != -1)
+	{
+		nb *= base;
+		nb += convert(str[i], base);
+		i++;
+	}
+	if (isneg)
+		return (nb *= -1);
+	return (nb);
+}
