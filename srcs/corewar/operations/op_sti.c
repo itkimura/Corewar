46dcb6ce741c495939be960ac5ee0cbef69de0cc
Thu Le <thule@Thus-MacBook-Pro-2.local>
2023-01-10 14:00:39 +0200

    add ops

--- srcs/corewar/operations/op_sti.c
+++ srcs/corewar/operations/op_sti.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_sti.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thle <thle@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 15:35:30 by thle              #+#    #+#             */
-/*   Updated: 2023/01/09 14:14:45 by thle             ###   ########.fr       */
+/*   Updated: 2023/01/10 13:46:27 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,5 @@ bool	op_sti(t_game *game, t_carriage *carriage)
 	pos = carriage->pc + 
 			((get_value(game, carriage, SECOND_ARG, true) + get_value(game, carriage, THIRD_ARG, true)) % IDX_MOD);
 	place_value(game, value, pos);
-
-	// ft_printf("second: %d\n", get_value(game, carriage, SECOND_ARG, true));
-	// ft_printf("third: %d\n", get_value(game, carriage, THIRD_ARG, true));
-	// ft_printf("value: %d\n", value);
-
 	return true;
 }
