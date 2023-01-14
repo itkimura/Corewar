f2fe69923a1deed60e7d70ff251ad51e540d082c
Thu Le <thule@Thus-MacBook-Pro-2.local>
2023-01-14 03:32:00 +0200

    update test.s

--- test.s
+++ test.s
@@ -1,8 +1,13 @@
 .name    "test"
 .comment "Invincible"
 
-
-	ldi r2, %18, r6
+#	live %-1
+	lld %-31251324, r2
+	st r2, -10
+	sti r2, -1, r4
+#	lld 4080, r2
+#	ld %12, r1
+#	ldi r2, %18, r6
 #	ld %42, r2
 #    aff r2
 #	st r1, 1
