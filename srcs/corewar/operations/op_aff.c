2171d7daecb7e70ba079feeaa02198c7a639edb0
Thu Le <thule@Thus-MacBook-Pro-2.local>
2023-01-14 03:27:14 +0200

    rm statement that updates remaning cycle

--- srcs/corewar/operations/op_aff.c
+++ srcs/corewar/operations/op_aff.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_aff.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thle <thle@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 15:36:42 by thle              #+#    #+#             */
-/*   Updated: 2023/01/10 11:15:22 by itkimura         ###   ########.fr       */
+/*   Updated: 2023/01/13 14:57:35 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@ bool op_aff(t_game *game, t_carriage *carriage)
 {
 	if (game->flags_value[FLAG_A] != INITIAL_VALUE)
 			ft_printf("%c\n", (char)carriage->registry[carriage->arg_value[0] - 1]);
-	carriage->remaining_cycle = g_op_tab[OP_AFF].cycles;
 	return (true);
 }
