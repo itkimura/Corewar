2c08967585557f2eb09e24439fdcae6e3216f9ca
Thu Le <thule@Thus-MacBook-Pro-2.local>
2023-01-24 15:38:23 +0200

    fixed check and split function

--- srcs/corewar/run_game.c
+++ srcs/corewar/run_game.c
@@ -6,42 +6,14 @@
 /*   By: thule <thule@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 16:18:49 by itkimura          #+#    #+#             */
-/*   Updated: 2023/01/24 13:29:40 by thule            ###   ########.fr       */
+/*   Updated: 2023/01/24 15:33:18 by thule            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-
-void run_check(t_game *game)
+static void set_ctd(t_game *game)
 {
-	t_carriage *carriage;
-	t_carriage *next;
-	t_carriage *prev;
-
-	carriage = game->carriage_head;
-	prev = NULL;
-	next = NULL;
-	while (carriage)
-	{
-		next = carriage->next;
-		if (carriage->live_performed == false)
-		{
-			if (game->flags_value[FLAG_L] == FO_DEATHS)
-			{
-				ft_printf("Process %d hasn't lived for %d cycles (CTD %d)\n",
-						  carriage->id, game->total_cycles - carriage->last_live_performed - 1,
-						  game->cycles_to_die);
-			}
-			kill_carriage(game, prev, carriage, next);
-		}
-		else
-		{
-			carriage->live_performed = false;
-			prev = carriage;
-		}
-		carriage = next;
-	}
 	if (game->total_lives >= NBR_LIVE || (game->total_checks % MAX_CHECKS == 0 && game->total_checks != 0))
 	{
 		game->cycles_to_die = game->cycles_to_die - CYCLE_DELTA;
@@ -52,7 +24,61 @@ void run_check(t_game *game)
 	game->total_checks++;
 }
 
-bool run_carriages(t_game *game)
+static void run_check(t_game *game)
+{
+	t_carriage *carriage;
+	t_carriage *next;
+	t_carriage *prev;
+	int limit;
+
+	carriage = game->carriage_head;
+	prev = NULL;
+	next = NULL;
+	limit = game->total_cycles - game->cycles_to_die - 1;
+	if (game->cycles_to_die < 0)
+		limit = game->total_cycles - 1;
+	while (carriage)
+	{
+		next = carriage->next;
+		if (carriage->last_live_performed <= limit)
+		{
+			print_flag_l_death(game, carriage);
+			kill_carriage(game, prev, carriage, next);
+		}
+		else
+			prev = carriage;
+		carriage = next;
+	}
+	set_ctd(game);
+}
+
+static bool perform_op(t_game *game, t_carriage *carriage)
+{
+	if (carriage->remaining_cycle <= 0)
+	{
+		carriage->statement_index = game->arena[carriage->pc] - 1;
+		if (is_op_code(carriage->statement_index) == true)
+			carriage->remaining_cycle = g_op_tab[carriage->statement_index].cycles;
+		else
+		{
+			carriage->pc = (carriage->pc + 1) % MEM_SIZE;
+			carriage->remaining_cycle = 1;
+		}
+	}
+	else if (carriage->remaining_cycle == 1)
+	{
+		if (get_arg_value(carriage, game->arena) == true)
+		{
+			if (g_op_tab[carriage->statement_index].f(game, carriage) == false)
+				return (false);
+		}
+		flag_l(game, carriage);
+		carriage->pc = carriage->next_statement_pc;
+	}
+	return (true);
+}
+
+static bool run_carriages(t_game *game)
 {
 	t_carriage *carriage;
 
@@ -60,32 +86,12 @@ bool run_carriages(t_game *game)
 	while (carriage)
 	{
 		carriage->remaining_cycle--;
-		if (carriage->remaining_cycle <= 0)
-		{
-			carriage->statement_index = game->arena[carriage->pc] - 1;
-			if (is_op_code(carriage->statement_index) == true)
-				carriage->remaining_cycle = g_op_tab[carriage->statement_index].cycles;
-			else
-			{
-				carriage->pc = (carriage->pc + 1) % MEM_SIZE;
-				carriage->remaining_cycle = 1;
-			}
-		}
-		else if (carriage->remaining_cycle == 1)
-		{
-			if (get_arg_value(carriage, game->arena) == true)
-			{
-				if (g_op_tab[carriage->statement_index].f(game, carriage) == false)
-					return (false);
-			}
-			flag_l(game, carriage);
-			carriage->pc = carriage->next_statement_pc;
-		}
+		if (perform_op(game, carriage) == false)
+			return false;
 		carriage = carriage->next;
 	}
 	return true;
 }
-
 
 bool run_game(t_game *game)
 {
@@ -95,23 +101,18 @@ bool run_game(t_game *game)
 	while (game->carriage_head != NULL)
 	{
 		if (game->flags_value[FLAG_L] == FO_CYCLES)
-			ft_printf("It is now cycle %d\n", game->total_cycles + 1);
+			ft_printf("It is now cycle %d\n", game->total_cycles);
 		if (print_dump(game) == false)
 			break;
-		if (game->flags_value[FLAG_DUMP] == game->total_cycles)
-		{
-			print_arena(game);
-			return (true);
-		}
 		run_carriages(game);
 		game->total_cycles++;
 		game->check_counter--;
-
 		if (game->cycles_to_die <= 0 || game->check_counter <= 0)
 			run_check(game);
 		index++;
 	}
 	if (game->carriage_head == NULL)
-		ft_printf("Contestant %d, \"%s\", has won !\n", (game->winner) * -1, game->players_in_order[(game->winner) * -1 - 1]->name);
+		ft_printf("Contestant %d, \"%s\", has won !\n", (game->winner) * -1,
+		game->players_in_order[(game->winner) * -1 - 1]->name);
 	return (true);
 }
