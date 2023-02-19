b59e7de6107fc4ea4d5ce287fc09271231f68ff2
Thi anh thu Le <thle@c2r1p6.hive.fi>
2023-02-19 16:08:45 +0200

    Add check_segfault.sh

--- eval_tests/check_segfault.sh
+++ eval_tests/check_segfault.sh
@@ -1,2 +1,25 @@
+#!/bin/bash
+
+#
+# File path configuration (Please change if you want!)
+#
 ASM_PATH="./"
-S_FILES_PAHT="/res"
+S_FILES_PATH="./resources/s_files/"
+
+#
+# Color scheme
+#
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+BOLD='\033[0;1m'
+NC='\033[0m'
+ac=$#
+
+S_FILES=`ls $S_FILES_PATH`
+for FILE in $S_FILES;
+do
+	echo -e "$YELLOW\n$S_FILES_PATH$FILE$NC"
+	./asm $S_FILES_PATH$FILE
+done
