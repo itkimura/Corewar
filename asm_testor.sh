99a254f7361f276bf8468285e5c967c598961936
Clement Cariou <wdclem@hotmail.fr>
2023-02-22 13:49:50 +0200

    fix on asm_testor.sh, add error_to_fix.txt list of error for asm error_files from other scripts

--- asm_testor.sh
+++ asm_testor.sh
@@ -75,18 +75,22 @@ if [ "$CLEANUP" == true ]; then
     if [ -f "error_asm_test.txt" ]; then
         rm "error_asm_test.txt"
     fi
+    echo -e "\033[35mAll files deleted\033[0m"
     exit 0
 fi
 
 # Create directories for output files
 if [ "$TEST_TYPE" == "valid" ]; then
-	mkdir -p our_valid_asm
-	mkdir -p og_valid_asm 
+    our_asm_dir="$(dirname "$0")/our_valid_asm"
+    mkdir -p "$our_asm_dir"
+    og_asm_dir="$(dirname "$0")/og_valid_asm"
+    mkdir -p "$og_asm_dir"
 elif [ "$TEST_TYPE" == "error" ]; then
-	mkdir -p our_error_asm
-	mkdir -p og_error_asm
+    our_asm_dir="$(dirname "$0")/our_error_asm"
+    mkdir -p "$our_asm_dir"
+    og_asm_dir="$(dirname "$0")/og_error_asm"
+    mkdir -p "$og_asm_dir"
 fi
-
 
 # Remove output files if they exist
 if [ -f valid_asm_test.txt ]; then
@@ -99,27 +103,32 @@ fi
 
 # Run asm and example_asm on all files in s_files directory with -valid option
 if [ "$TEST_TYPE" == "valid" ]; then
-	echo "Testing with asm and example_asm with valid"
-	for file in resources/s_files/*; do
-		echo "***OUR ASM***\n" >> valid_asm_test.txt
-		./asm "$file" > "$(dirname "$0")/our_valid_asm/$(basename "${file%.*}").cor"
-    	./asm $file >> valid_asm_test.txt
-		echo "***OG ASM***\n" >> valid_asm_test.txt
-		./resources/example_asm "$file" > "$(dirname "$0")/og_valid_asm/$(basename "${file%.*}").cor"
-    	./resources/example_asm $file >> valid_asm_test.txt
-	done
-	echo "Tests completed. Results can be found in valid_asm_test.txt"
+    echo -e "\033[35mTesting with asm and example_asm with valid\033[0m"
+    for file in resources/s_files/*.s; do
+        echo -e "***\033[32mOUR ASM***\n"\033[0m >> valid_asm_test.txt
+		./asm "$file" 2>&1 | tee -a valid_asm_test.txt > /dev/null
+		echo -e "\n" >> valid_asm_test.txt 
+        mv "${file%.s}.cor" our_valid_asm/
+        echo -e "***\033[31mOG ASM***\n"\033[0m >> valid_asm_test.txt
+		./resources/asm "$file" 2>&1 | tee -a valid_asm_test.txt > /dev/null
+		echo -e "\n" >> valid_asm_test.txt 
+        mv "${file%.s}.cor" og_valid_asm/
+    done
+    echo -e "\033[35mTests completed. Results can be found in valid_asm_test.txt\033[0m"
 
 # Run asm and example_asm on all files in asm_error_files directory with -error option
+#
 elif [ "$TEST_TYPE" == "error" ]; then
- 	echo "Testing with asm and example_asm error"
-	for file in eval_tests/asm_error_files/*; do
-		echo "***OUR ASM***\n" >> error_asm_test.txt   
-		./asm "$file" > "$(dirname "$0")/our_error_asm/$(basename "${file%.*}").cor"
-    	./asm $file >> error_asm_test.txt
-		echo "***OG ASM***\n" >> error_asm_test.txt   
-		./resources/example_asm "$file" > "$(dirname "$0")/og_error_asm/$(basename "${file%.*}").cor"
-    	./resources/example_asm $file >> error_asm_test.txt
-	done
-	echo "Tests completed. Results can be found in error_asm_test.txt"
+ 	echo -e "\033[35mTesting with asm and example_asm error\033[0m"
+    for file in eval_tests/asm_error_files/*.s; do
+        echo -e "***\033[32mOUR ASM***\n\033[0m" >> error_asm_test.txt
+		./asm "$file" 2>&1 | tee -a error_asm_test.txt > /dev/null
+		echo -e "\n" >> error_asm_test.txt 
+        mv "${file%.s}.cor" our_error_asm/
+        echo -e "***\033[31mOG ASM***\n\033[0m" >> error_asm_test.txt
+		./resources/asm "$file" 2>&1 | tee -a error_asm_test.txt > /dev/null
+		echo -e "\n" >> error_asm_test.txt 
+        mv "${file%.s}.cor" og_error_asm/
+    done
+	echo -e "\033[35mTests completed. Results can be found in error_asm_test.txt\033[0m"
 fi
